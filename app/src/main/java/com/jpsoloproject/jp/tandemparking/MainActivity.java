ece8a03ccc0d8d244f8e2cf9353de7b6f2b61f60
JP <jobethpalacio@outlook.com>
2017-01-10 03:45:48 -0800

    Initial commit

---
+++ app/src/main/java/com/jpsoloproject/jp/tandemparking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jpsoloproject.jp.tandemparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/jpsoloproject/jp/tandemparking/ManualEntry.java
@@ -0,0 +1,8 @@
+package com.jpsoloproject.jp.tandemparking;
+
+/**
+ * Created by JP on 1/10/2017.
+ */
+
+public class ManualEntry {
+}
